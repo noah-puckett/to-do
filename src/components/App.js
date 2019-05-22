bc307c3700b8ac1f06213adfc7b50195f38dfd97
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-22 10:30:38 -0700

    finished first pass of comments on App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,8 @@ import AddItem from '../components/AddItem.js';
 import todoData from '../../data/todo-data.js';
 import TodoList from './TodoList.js';
 
-
 //THIS FILE RENDERS OUR WEBSITE DYNAMICALLY
-//classes don't use commas!!!
+//classes don't use commas, don't forget!
 class App extends Component {
     
     //below we are rendering the whole 'app' of our website,
@@ -27,34 +26,30 @@ class App extends Component {
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
         //this grabs data and renders our list items
-        //wrote this out to be intentionally verbose, I know it can be shortened but I'm still confused 
+        //wrote this out to be intentionally verbose, I know it can be shortened but I'm not ready for brevity!
         const props = { 
             todoData: todoData 
         };
 
         //we assign a new instance of our AddItem component to a var 'addItem'
-        //I THINK, because AddItem inherits properties from Component, we OVERRIDE those by passing in AddItem({ onAdd:... })?
-        //the onAdd OBJECT KEY has a VALUE 
+        //Because AddItem inherits properties from Component, we OVERRIDE/ADD ONTO those by passing in AddItem({ onAdd:... })
+        //the onAdd OBJECT KEY has a VALUE that is an ANONYMOUS FUNCTION, that we give an argument of newItem, and the code to run INSIDE the function
+        //is adding (pushing to the 0 index, unshift) newItem, then updating the TodoList COMPONENT'S PROPS!!! with the updated todoData ARRAY OBJECT
         const addItem = new AddItem({
             onAdd: (newItem) => { todoData.unshift(newItem); todoList.update({ todoData }); }
         });
 
-        //could you write it like:
-        // const addItem2 = new AddItem(
-        //     { onAdd: function onAdd(newItem) {
-        //         todoData.unshift(newItem);
-        //         todoList.update( {todoData: todoData} )
-        //} });
-
-
+        //here we follow the pattern of processing the HTML string into DOM elements, then appending that DOM element to a PARENT DOM element
         const addItemDOM = addItem.render();
         main.appendChild(addItemDOM);
         
-
+        //we initialize our todoList INSTANCE of our TodoList COMPONENT, and set its PROPERTIES to the todoData ARRAY (const props... above on line 32)
+        //follow the same pattern of rendering our HTML to DOM and appending 
         const todoList = new TodoList(props);
         const todoListDOM = todoList.render();
         main.appendChild(todoListDOM);
 
+        //finally, we return the whole DOM we've been appending to
         return dom;
     }
 
