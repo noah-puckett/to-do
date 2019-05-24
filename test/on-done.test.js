215abe88c8d765cd4652180e9fc84a6f7573a4a1
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-24 09:20:37 -0700

    moved onDone to own file

--- test/on-done.test.js
+++ test/on-done.test.js
@@ -1,25 +1,17 @@
+import onDone from '../src/on-done.js';
 const test = QUnit.test;
 
 QUnit.module('onDone callback function test 1');
 
-function onDone(listItem) {
-    listItem.completed = true;
-    return listItem;
-}
+test('onDone function changes completed from false to true', function(assert) {
 
-test('Can I make a test for onDone?', function(assert) {
-    //so I want an event listener that, ON CHECKED, changes the listItem object to {completed: true}?
-
-    //arrange
     const listItem = {
         text: 'test name',
         completed: false
     };
 
-
-    //act
     const result = onDone(listItem);
-    //assert
+
     assert.deepEqual(result, {
         text: 'test name',
         completed: true
